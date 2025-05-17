72202524c0a44c32b6ec3835f00fe5bbf211fe2c
MatheusGPorto <matheusporto97@gmail.com>
2025-05-16 21:10:04 -0300

    Iniciando projeto

---
+++ style.php
@@ -0,0 +1,15 @@
+<style> 
+    .vermelho {background-color: red;}
+    
+</style>
+
+<?php
+$estilo = 'vermelho';
+?>
+
+<body class=<?php echo $estilo ?>>
+    
+    <h1>Hey, this is my first website</h1>
+    
+
+</body>
